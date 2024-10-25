304b9e33ee3004c61bfd09d7866174ed8a392c81
Erag0n001 <148992045+Erag0n001@users.noreply.github.com>
2024-10-25 03:14:37 -0400

    Fixed issue where program would crash with missing file

--- Server/HTTP Server test/HTTP Server test/Program.cs
+++ Server/HTTP Server test/HTTP Server test/Program.cs
@@ -9,6 +9,7 @@ namespace HTTP_Server {
         public static List<PersonData> people = new List<PersonData>();
         static void Main()
         {
+            if (!Directory.Exists(Path.Combine(Assembly.GetExecutingAssembly().Location, "..", "Data"))) Directory.CreateDirectory(Path.Combine(Assembly.GetExecutingAssembly().Location, "..", "Data"));
             if (File.Exists(Program.path))
             {
                 List<byte[]> byteData = Serializer.SerializeFromFile<List<byte[]>>(Program.path).ToList();
